76aaefc96255586062685e895102eb2f0a54bc5e
Sumon <sumonece15@gmail.com>
2020-11-11 20:26:56 +0600

    Boolean Keyword

---
+++ src/BooleanKeyword.java
@@ -0,0 +1,21 @@
+
+public class BooleanKeyword {
+
+	public static void main(String[] args) {
+		
+		int a=25;
+		int b=30;
+		
+		boolean c= true;
+		boolean d= false;
+		
+		if(a<b){
+			System.out.println(c);
+		}
+
+		else{
+			System.out.println(d);
+		}
+	}
+
+}
